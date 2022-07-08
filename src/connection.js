c5b793f15624a2950139a46df6f9341b251c2e9d
rdfconza <104052003+rdfconza@users.noreply.github.com>
2022-07-08 12:37:56 -0500

    avanse APInodejs

---
+++ src/connection.js
@@ -0,0 +1,6 @@
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://localhost:27017/storedb',{
+   //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //useCreateIndex: true
+}).then(db =>console.log('Connection establishe successflully'))
